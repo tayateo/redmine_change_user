7f610f14c9649726104d8c02229a37f3b53e8d41
a.fedorova <a.fedorova@elins.ru>
2017-04-28 15:27:51 +0300

    refactoring

--- app/controllers/vhod_change_user_controller.rb
+++ app/controllers/vhod_change_user_controller.rb
@@ -2,12 +2,12 @@ class VhodChangeUserController < ApplicationController
 
   def change
 
-    new_user = User.find(params['id'])
+    change_user = User.find(params['id'])
 
-    session[:user_id] = new_user.id
-    session[:tk] = new_user.generate_session_token
+    session[:user_id] = change_user.id
+    session[:tk] = change_user.generate_session_token
     session[:turn_back_to] = User.current.id
-    session[:previous_user_name] = User.current.login
+    session[:initial_user_name] = User.current.login
 
     redirect_to '/'
 
@@ -15,12 +15,12 @@ class VhodChangeUserController < ApplicationController
 
   def turn_back
 
-    previous_user = User.find(session[:turn_back_to])
+    initial_user = User.find(session[:turn_back_to])
 
-    session[:user_id] = previous_user.id
-    session[:tk] = previous_user.generate_session_token
+    session[:user_id] = initial_user.id
+    session[:tk] = initial_user.generate_session_token
     session[:turn_back_to] = nil
-    session[:previous_user_name] = nil
+    session[:initial_user_name] = nil
 
     redirect_to '/users'
 
