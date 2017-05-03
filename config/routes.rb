189785dbd9758fafa01ffd5f1c638283aa0f877a
a.fedorova <a.fedorova@elins.ru>
2017-05-03 14:46:35 +0300

    new syntax in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 RedmineApp::Application.routes.draw do
-
-  match '/change/user/:id', :to => 'vhod_change_user#change', :as => 'vhod_change_user', via: [:get, :post]
-  match '/turn_back_to/user/:id', :to => 'vhod_change_user#turn_back', :as => 'vhod_turn_back', via: [:get, :post]
-
+  match '/change/user/:id',
+        to: 'vhod_change_user#change',
+        as: 'vhod_change_user',
+        via: [:get, :post]
+  match '/turn_back_to/user/:id',
+        to: 'vhod_change_user#turn_back',
+        as: 'vhod_turn_back',
+        via: [:get, :post]
 end
