e8044dcc9558a6ed6379d9bf8fe2d47fcea45aa2
a.fedorova <a.fedorova@elins.ru>
2017-05-03 15:05:27 +0300

    unnecessary white spaces

--- app/controllers/vhod_change_user_controller.rb
+++ app/controllers/vhod_change_user_controller.rb
@@ -1,24 +1,19 @@
 class VhodChangeUserController < ApplicationController
-
   def change
     change_user = User.find(params['id'])
-
     session[:user_id] = change_user.id
     session[:tk] = change_user.generate_session_token
     session[:turn_back_to] = User.current.id
     session[:initial_user_name] = User.current.login
-
     redirect_to '/'
   end
 
   def turn_back
     initial_user = User.find(session[:turn_back_to])
-
     session[:user_id] = initial_user.id
     session[:tk] = initial_user.generate_session_token
     session[:turn_back_to] = nil
     session[:initial_user_name] = nil
-
     redirect_to '/users'
   end
 end
