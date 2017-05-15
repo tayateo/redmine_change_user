0bb37ba3070e2f5611d6b26a24b1e244a4456e50
a.fedorova <a.fedorova@elins.ru>
2017-05-15 15:11:13 +0300

    test refactoring

--- spec/features/mutations_feature_spec.rb
+++ spec/features/mutations_feature_spec.rb
@@ -1,53 +1,39 @@
 require File.expand_path('../../rails_helper', __FILE__)
 
-describe 'Мутация', js: true do
-  let!(:user) { FactoryGirl.create(:user, department: Department.new) }
-  let(:admin) { FactoryGirl.create(:admin, login: 'admin', department: Department.new) }
+describe 'При мутации', js: true do
+  let(:department) { create(:department) }
+  let!(:user) { create(:user, department: department) }
+  let(:init_user) { create(:admin, login: 'admin', department: department) }
+  before :each do
+    login_user(init_user.login, 'foo')
+    visit users_path
+  end
 
-  context 'при наличии админских полномочий' do
-
-    before :each do
-      login_user(admin.login, 'foo')
-    end
-
-    describe 'ссылка переключения пользователя' do
-      it 'должна существовать' do
-        visit users_path
-        expect(page).to have_selector('.change-user > a', text: "Стать #{user.login}")
-      end
-    end
-
-    describe 'нажатие на ссылку Стать <username>' do
-      it 'вызывает появление идентификационной подписи в правом верхнем углу' do
-        visit users_path
+  context 'если пользователь админ' do
+    describe 'то отображается ссылка переключения пользователя' do
+      it 'после нажатия на которую появляется идентификатор другого пользователя и кнопка обратного переключения' do
         click_link "Стать #{user.login}"
-        expect(page).to have_selector('#loggedas', text: "Вошли как #{user.login}")
-      end
-
-      it 'вызывает появление кнопки снова стать админом' do
-        visit users_path
-        click_link "Стать #{user.login}"
-        expect(page).to have_selector('#turn-back-panel > a', text: "Снова стать #{admin.login}")
+        expect(page).to have_css('#turn-back-panel > a', text: "Снова стать #{init_user.login}")
+        expect(page).to have_css('#loggedas', text: "Вошли как #{user.login}")
       end
     end
 
-    describe 'возможность обратной мутации' do
+    describe 'ссылка обратного переключения на админа' do
       it 'не сохраняется после завершения сеанса' do
-        visit users_path
         click_link "Стать #{user.login}"
         click_link "Выйти"
         login_user(user.login, 'foo')
-        expect(page).not_to have_selector('#turn-back-panel > a', text: "Снова стать #{admin.login}")
+        expect(page).to have_no_link("Снова стать #{init_user.login}")
       end
     end
   end
 
-  context 'при отсутствии админских полномочий' do
+  context 'если пользователь не админ' do
+    let(:init_user) { user }
+
     describe 'ссылка переключения пользователя' do
-      it 'не должна существовать' do
-        login_user(user.login, 'foo')
-        visit users_path
-        expect(page).not_to have_selector('.change-user')
+      it 'не существует' do
+        expect(page).to have_no_css('.change-user')
       end
     end
   end
