f8588fda0521a3cd0640529d7b5898a3d76ad5f1
a.fedorova <a.fedorova@elins.ru>
2017-06-05 16:19:22 +0300

    рефакторинг названий хуков (чтобы было понятно, где хук)

--- lib/vhod_change_user/hooks.rb
+++ lib/vhod_change_user/hooks.rb
@@ -1,8 +1,8 @@
 class VhodChangeUser::Hooks < Redmine::Hook::ViewListener
   render_on :view_layouts_base_body_top,
-            partial: 'hooks/vhod_change_user/turn_back'
+            partial: 'hooks/vhod_change_user/view_layouts_base_body_top'
   render_on :view_layouts_base_html_head,
-      partial: 'hooks/vhod_change_user/change_user_style'
+      partial: 'hooks/vhod_change_user/view_layouts_base_html_head'
   render_on :view_account_left_bottom,
-            partial: 'hooks/vhod_change_user/mutate_on_user_page'
+            partial: 'hooks/vhod_change_user/view_account_left_bottom'
 end
